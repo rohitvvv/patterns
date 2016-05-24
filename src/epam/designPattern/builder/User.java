aa35bd55f2d04640ffce4339585d8488d6009438
Rohit <rohitvvv@gmail.com>
2016-05-24 14:07:01 +0530

    Adding maven support. Adding test class

--- src/epam/designPattern/builder/User.java
+++ src/epam/designPattern/builder/User.java
@@ -5,45 +5,77 @@ package epam.designPattern.builder;
  *
  */
 public class User {
-   private String firstName;
-   private String lastName;
-   private int age;
-   private int phone;
-   private User(UserBuilder builder){
-	   this.firstName=builder.firstName;
-	   this.lastName=builder.lastName;
-	   this.age=builder.age;
-	   this.phone=builder.phone;
-   }
-   public int getAge(){
-	   return age;
-   }
-   public int phone(){
-	   return phone;
-   }
-   public static class UserBuilder{
-	   private String firstName;
-	   private String lastName;
-	   private int age;
-	   private int phone;
-	   public UserBuilder(String fname,String lname){
-		   this.firstName=fname;
-		   this.lastName=lname;
-	   }
-	   public UserBuilder setAge(int age){
-		   this.age=age;
-		   return this;
-	   }
-	   public UserBuilder setPhone(int phone){
-		   this.phone=phone;
-		   return this;
-	   }
-	   public User build(){
-		   return new User(this);
-	   }
-   }
-   public static void main(String...args){
-	   User user = new User.UserBuilder("rohit","vaidya").setAge(10).setPhone(123).build();
-	   System.out.println(user.getAge());
-   }
+  private String firstName;
+  private String lastName;
+  private int age;
+  private int phone;
+  /**
+   * Prevent access to the constructor
+   * Use builder to construct the object
+   * @param builder
+   */
+  private User(UserBuilder builder){
+     this.firstName=builder.firstName;
+     this.lastName=builder.lastName;
+     this.age=builder.age;
+     this.phone=builder.phone;
+  }
+  /**
+   * Accessor for Age
+   * @return Integer Age
+   */
+  public int getAge(){
+	  return age;
+  }
+  /**
+   * Accessor for phone
+   * @return
+   */
+  public int getPhone(){
+	  return phone;
+  }
+  /**
+   * Accessor for First Name
+   * @return
+   */
+  public String getFirstName(){
+	  return firstName;
+  }
+  /**
+   * Accessor for Last Name
+   * @return
+   */
+  public String getLastName(){
+	  return lastName;
+  }
+  
+  public static class UserBuilder{
+	  private String firstName;
+	  private String lastName;
+	  private int age;
+	  private int phone;
+	  public UserBuilder(String firstName,String lastName){
+		  this.firstName=firstName;
+		  this.lastName=lastName;
+	  }
+	  public UserBuilder setAge(int age){
+		  this.age=age;
+		  return this;
+	  }
+	  public UserBuilder setPhone(int phone){
+		  this.phone=phone;
+		  return this;
+	  }
+	  public User build(){
+		  return new User(this);
+	  }
+  }
+  
+  public static void main(String...strings){
+	  //Create new instance of builder which will build the User object
+	  User rohit = new User.UserBuilder("Rohit","Vaidya").setAge(10).setPhone(123456).build();
+      System.out.println(rohit.firstName);
+      System.out.println(rohit.age);
+  }
+
 }
